e71357e6dc6eafe1db5fa7470855d1f4e8647542
Mihajlo Colic <colicmihajlo@gmail.com>
2026-03-09 20:12:29 +0100

    Updated components

--- app/components/BookList.tsx
+++ app/components/BookList.tsx
@@ -12,17 +12,21 @@ export default function BookList() {
     const [books, setBooks] = useState<Book[]>([]);
 
     useEffect(() => {
+        
         getBooks().then((data) => {
-            setBooks(data)
+            if(!data) return;
+            
+            setBooks(data);
         })
+        
     }, []);
 
     
     return (
-        <div className="flex flex-col bg-red-900 rounded-md p-2">
+        <div className="flex max-h-max flex-col bg-red-900 rounded-md p-2">
             <h3 className="text-4xl">Књиге</h3>
-            <ul className="flex flex-col gap-1 text-l p-2">
-               {books.map(book => (
+            <ul className="flex flex-col gap-1 text-xl">
+               {books.map((book) => (
                 <li key={book.id}>{book.bookName}</li>
                ))}
             </ul>
--- app/components/SearchBar.tsx
+++ app/components/SearchBar.tsx
@@ -1,24 +1,29 @@
 'use client';
 
-import { useState, useEffect } from 'react';
+import { useState } from 'react';
 import { searchVerses } from "../lib/api";
 
 interface Verse {
     id: number,
     verseNumber: number,
     verseText: string,
+    bookId: number,
     chapterNumber: number,
     chapterId: number,
-    bookId: number
+    bookName: string
 }
 
 export default function SearchBar() {
     const [search, setSearch] = useState('');
     const [verses, setVerses] = useState<Verse[]>([]);
 
-    useEffect(() => {
-        searchVerses(search).then((data) => setVerses(data));
-    })
+
+    async function handleSearch() {
+        if(!search.trim()) return;
+        
+        searchVerses(search).then((data) => {console.log(data); setVerses(data);});
+    }
+
     return (
         <div>
             <div className="flex items-center justify-center gap-2">
@@ -26,18 +31,22 @@ export default function SearchBar() {
                     type="text"
                     placeholder="Претражи стихове..."
                     className="flex-1 rounded-md border border-white-300 px-4 py-2 focus:border-red-500 focus:ring focus:ring-red-100 focus:ring-opacity-50"
+                    id="searchInput"
                     onChange={(e) => setSearch(e.target.value)}
+                    onKeyDown={(e) => e.key === 'Enter' && handleSearch()}
                 />
-                <button className="rounded-md bg-red-950 px-4 py-2 text-white hover:bg-red-900 focus:outline-none focus:ring focus:ring-red-200 focus:ring-opacity-50">
+                <button className="rounded-md bg-red-950 px-4 py-2 text-white hover:bg-red-900 focus:outline-none focus:ring focus:ring-red-200 focus:ring-opacity-50"
+                onClick={() => handleSearch()}
+                >
                     Претражи
                 </button>
             </div>
             <div className="flex-1 flex-col gap-4 rounded-md p-4">
-                <h3 className="text-2xl p-4">Резултати претраге</h3>
+                <h3 className="text-2xl p-4">{verses.length < 1 ? "" : "Резултати претраге: " + verses.length + " стихова."}</h3>
                 <div className="flex flex-col gap-2 text-2xl">
                     {verses.map(verse => (
                         <div key={verse.id} className="searchResult rounded-md border border-gray-300 p-4">
-                            <h4 className="text-lg font-semibold">{verse.chapterNumber}:{verse.verseNumber}</h4>
+                            <h4 className="text-lg font-semibold">{verse.bookName} {verse.chapterNumber}:{verse.verseNumber}</h4>
                             <p>{verse.verseText}</p>
                         </div>
                     ))}
