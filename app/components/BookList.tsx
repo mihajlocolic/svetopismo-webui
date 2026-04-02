d52ef1a11b28ff7a6d0b3785060cc41e9601b3fd
Mihajlo Colic <colicmihajlo@gmail.com>
2026-04-02 20:03:11 +0200

    Added chapter grid upon book selection and deselect button

--- app/components/BookList.tsx
+++ app/components/BookList.tsx
@@ -21,6 +21,7 @@ export default function BookList() {
 
     const [books, setBooks] = useState<Book[]>([]);
     const [chapters, setChapters] = useState<Chapter[]>([]);
+    const [book, setBook] = useState<Book | null>(null);
     useEffect(() => {
         
         getBooks().then((data) => {
@@ -32,26 +33,48 @@ export default function BookList() {
     }, []);
 
 
-    async function handleBookSelect(bookId:number) {
-        console.log(`Looking up chapters for book ID ${bookId}`);
+    async function handleBookSelect(Book: Book) {
+        console.log(`Looking up chapters for book ID ${Book.bookId}`);
+        setBook(Book);
 
-        getBookChapters(bookId).then((data) => {
+        getBookChapters(Book.bookId).then((data) => {
             if(data) {
                 console.log(data);
                 setChapters(data);
             } else {
+
                 console.log("No chapters found for this book.");
             }
             
         });
     }
 
+    async function handleChapterSelect(chapter: Chapter) {
+        console.log(`Selected chapter ${chapter.chapterNumber} of book ID ${chapter.bookId}`);
+    }
+
+    async function handleBookDeselect() {
+        setBook(null);
+        setChapters([]);
+    }
+
     return (
         <div className="flex max-h-max flex-col bg-red-900 rounded-md p-2">
+            {chapters && (
+                <div className="flex flex-col gap-1">
+                    {book && ( <h3 className="text-3xl">{book.bookName} - поглавља</h3>)}
+                    <ul className="flex flex-wrap flex-row text-2xl gap-2 w-100">
+                        {chapters.map((chapter) => (
+                            <li className="chapterItem" key={chapter.chapterId} onClick={() => handleChapterSelect(chapter)}>{` ${chapter.chapterNumber} `}</li>
+                        ))}
+                        {book && (<li id="deselectButton" className="rounded-md bg-red-950 px-2 py-2 text-white text-xl hover:bg-red-800 focus:outline-none focus:ring focus:ring-red-200 focus:ring-opacity-50 select-none" onClick={() => handleBookDeselect()}>Deselect</li>)}
+                    </ul>   
+                </div>
+            )}  
             <h3 className="text-4xl">Књиге</h3>
             <ul className="flex flex-col gap-1 text-xl">
                {books.map((book) => (
-                <li key={book.bookId} onClick={async () => handleBookSelect(book.bookId)}>{book.bookName}</li>
+                <li className="bookItem" key={book.bookId} onClick={async () => handleBookSelect(book)}>{book.bookName}</li>
                ))}
             </ul>
         </div>
